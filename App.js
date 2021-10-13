bc79644378aa546cccb584c690ca0d346c2d1d35
ihzarizkyk <ihzarizky30@gmail.com>
2021-10-13 20:18:46 +0700

    first commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text> Mochammad Ihza Rizky Karim - Ngoding Way </Text>
       <StatusBar style="auto" />
     </View>
   );
